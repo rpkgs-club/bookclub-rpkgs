f992001681799483e47dd5c13ba4bf7ba4d1c217
Wlodek Bzyl <matwb@univ.gda.pl>
2020-11-04 18:23:10 +0100

    update README.md, add _common.R

---
+++ 2-the_whole_game/README.md
@@ -0,0 +1,20 @@
+# Fixes for chapter 2: The whole game
+
+```r
+packageVersion("devtools")
+## [1] ‘2.3.2’
+options(buildtools.check=NULL)
+devtools::install_github("r-lib/devtools")
+packageVersion("devtools")
+# [1] ‘2.3.1.9000’
+```
+
+```r
+library(devtools)
+library(tidyverse)
+library(fs)
+
+dir_info(all = TRUE, regexp = "^[.]git$") %>%
+  select(path, type)
+```
+
---
+++ 2-the_whole_game/_common.R
@@ -0,0 +1,21 @@
+# 2020.11.05
+# borrowed from git@github.com:hadley/r4ds.git
+
+# set.seed(2020)
+# options(digits = 3)
+
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  cache = TRUE,
+  out.width = "100%",
+  fig.path = "figures/",
+  fig.align = 'center',
+  fig.width = 12,
+  fig.asp = 0.618,  # 1 / phi
+  fig.show = "hold",
+  rows.print = 12
+)
+
+# options(dplyr.print_min = 6, dplyr.print_max = 6)
+# options(tibble.print_max = 16, tibble.print_min = 16)
